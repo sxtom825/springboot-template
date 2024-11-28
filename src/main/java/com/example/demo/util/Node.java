1abc68d31736574968b3a71781fae2dc9b8646f3
song <111142@163.com>
2024-11-28 13:27:44 +0800

    init

---
+++ src/main/java/com/example/demo/util/Node.java
@@ -0,0 +1,21 @@
+package com.example.demo.util;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Node<E> {
+
+    private E value;
+
+    volatile Node<E> next;
+
+    public Node(E value) {
+        this.value = value;
+    }
+
+    void ssss(){
+        Executor executor = Executors.newSingleThreadExecutor();
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+    }
+}
---
+++ src/main/java/com/example/demo/util/Test3.java
@@ -0,0 +1,60 @@
+package com.example.demo.util;
+
+
+
+import org.graalvm.collections.LockFreePrefixTree;
+import org.springframework.util.StopWatch;
+
+import java.util.Objects;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+public class Test3 {
+
+
+    public static void main(String[] args) {
+
+
+//        BlockingDeque<String> blockingDeque = new LinkedBlockingDeque<>(10);
+//        blockingDeque.offer("");
+//
+//
+//        try {
+//            blockingDeque.take();
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        IntStream.rangeClosed(0, 10).boxed().map(i -> new Thread(" th1 " + i) {
+//            @Override
+//            public void run() {
+//
+//            }
+//        }).forEach(Thread::start);
+
+        ConcurrentLinkedQueue<Object> queue = new ConcurrentLinkedQueue<>();
+        queue.add(new Object());
+
+        Object object = queue.poll();
+        int loops = 0;
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        while (true) {
+
+
+            queue.add(object);
+            queue.remove(object);
+            loops++;
+        }
+
+
+    }
+}
